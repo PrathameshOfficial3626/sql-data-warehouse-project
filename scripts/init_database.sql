2ef561494cf470f99023ac368ca9f3b1dcd8525d
PRATHAMESH SHINDE <prathameshofficial3626@gmail.com>
2025-06-18 21:58:06 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,16 @@
+---Create Database 'Datawarehouse'
+USE master;
+
+CREATE DATABASE DataWarehouse;
+
+--Using Database---
+USE DataWarehouse;
+
+
+
+---Creating Schema-----
+Create schema bronze;
+GO
+Create schema silver;
+GO
+Create schema gold;
